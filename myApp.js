9ea9df2eff3df72574c9ec04f9281a5152d6d7c2
Brando Collins <ghost.rkrv3@gmail.com>
2023-01-26 12:03:15 -0600

    Updated line

--- myApp.js
+++ myApp.js
@@ -22,7 +22,7 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
-app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trustid-cdn.com"] }}));
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }}));
 
 
 
